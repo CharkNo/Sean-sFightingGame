ad843deddd7a10ffbdbf9648e7745a454561cb3b
Carlos Ortiz <ortizc@etsu.edu>
2022-12-15 17:29:29 -0500

    wip
    fixing animations

--- js/classes.js
+++ js/classes.js
@@ -1,18 +1,44 @@
 class Sprite {
-	constructor({position, imageSrc}) {
+	constructor({position, imageSrc, scale = 1, frameMax = 1}) {
 		this.position = position
 		this.width = 50
 		this.height = 150
 		this.image = new Image()
 		this.image.src = imageSrc
+		this.scale = scale
+		this.frameMax = frameMax
+		this.framesCurrent = 0
+		this.framesElapsed = 0
+		this.framesHold = 10
 	}
 
 	draw() {
-		c.drawImage(this.image, this.position.x, this.position.y)
+		c.drawImage(
+			this.image, 
+			this.framesCurrent * (this.image.width / this.frameMax),
+			0,
+			this.image.width / this.frameMax, 
+			this.image.height,
+			this.position.x, 
+			this.position.y, 
+			(this.image.width / this.frameMax) * this.scale, 
+			this.image.height * this.scale
+			)
 	}
 
 	update() {
 		this.draw()
+		this.framesElapsed++;
+
+		if(this.framesElapsed % this.framesHold === 0)
+		{
+			if(this.framesCurrent < this.frameMax - 1) {
+				this.framesCurrent++
+			} else {
+				this.framesCurrent = 0
+			}
+		}
+		
 		
 	}
 
@@ -65,7 +91,7 @@ class Fighter {
 		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
 
-		if(this.position.y + this.height + this.velocity.y >= canvas.height) {
+		if(this.position.y + this.height + this.velocity.y >= canvas.height - 97) {
 			this.velocity.y = 0
 			this.numberOfJumps = 0
 		} else this.velocity.y += gravity
